75e7424a11f81f53261f195997022299ec7cf796
asdhq <1109005109@qq.com>
2021-03-16 20:38:23 +0800

    first commit

---
+++ src/main/java/com/csu/bdkei/dxrj/DxrjApplication.java
@@ -0,0 +1,13 @@
+package com.csu.bdkei.dxrj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DxrjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csu/bdkei/dxrj/DxrjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csu.bdkei.dxrj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DxrjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
